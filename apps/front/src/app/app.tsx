ba6795e05d7ad49b65191dc7ac8d6ab81db811aa
maxim darre <maxim.darre@gmail.com>
2021-10-09 14:24:09 -0400

    initialize nx workspace

---
+++ apps/front/src/app/app.tsx
@@ -0,0 +1,12 @@
+import styles from './app.module.css';
+
+import { ReactComponent as Logo } from './logo.svg';
+import star from './star.svg';
+
+export function App() {
+  return (
+    <div>Coucou Océane</div>
+  )
+}
+
+export default App;

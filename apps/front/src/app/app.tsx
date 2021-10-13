6cd97fe4d2bf627ce3b6e6b78bf5ad42cbc510df
oceanesch <oceane.sch@gmail.com>
2021-10-13 10:39:05 -0400

    add material ui library

--- apps/front/src/app/app.tsx
+++ apps/front/src/app/app.tsx
@@ -1,13 +1,13 @@
-import styles from './app.module.css';
-import { Button } from 'antd';
-import { Fragment } from 'react'
-
+import { Fragment } from 'react';
+import Button from '@mui/material/Button';
 
 export function App() {
-  return <Fragment>
-  <div>Coucou Océane</div>;
-  <Button type="default">Antd Btn</Button>
-  </Fragment>
+  return (
+    <Fragment>
+      <div>Coucou Océane</div>
+      <Button variant="contained">Ze suis la plus forte</Button>
+    </Fragment>
+  );
 }
 
 export default App;

0d954751b3ef1bb48ad7f5d499cb100177df5715
oceanesch <oceane.sch@gmail.com>
2021-10-19 11:06:36 -0400

    add a themeprovider to the navigation

--- apps/front/src/components/Layout/MainNavigation.tsx
+++ apps/front/src/components/Layout/MainNavigation.tsx
@@ -3,22 +3,26 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Toolbar from '@mui/material/Toolbar';
 import Stack from '@mui/material/Stack';
+import { ThemeProvider } from '@mui/material';
+import theme from '../../theme/ThemeProvider';
 
 const MainNavigation = () => {
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography component="div" sx={{ flexGrow: 1 }}>
-          VegeStation
-        </Typography>
-        <Stack direction="row" spacing={2}>
-          <Button variant="contained">Login</Button>
-          <Button variant="outlined" color="inherit">
-            Sign in
-          </Button>
-        </Stack>
-      </Toolbar>
-    </AppBar>
+    <ThemeProvider theme={theme}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography component="div" sx={{ flexGrow: 1 }}>
+            VegeStation
+          </Typography>
+          <Stack direction="row" spacing={2}>
+            <Button variant="contained">Login</Button>
+            <Button variant="outlined" color="inherit">
+              Sign in
+            </Button>
+          </Stack>
+        </Toolbar>
+      </AppBar>
+    </ThemeProvider>
   );
 };
 
